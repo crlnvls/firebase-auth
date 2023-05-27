c003db708e41a6aab9f8f905704f5ab4dcec8bf2
“crlnvls” <cveloso@hotmail.co.uk>
2023-05-27 20:27:21 +0100

    Add links on home page

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Home</h1>
+      <Link to="/login">Login</Link>
+      <br />
+      <Link to="/register">Register</Link>
+    </>
+  );
+};
+
+export default Home;
