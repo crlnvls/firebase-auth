8b8bc4614989985540ebf63f903c0ca1aa4ca30c
“crlnvls” <cveloso@hotmail.co.uk>
2023-05-27 20:56:00 +0100

    Add navigate when logout

--- src/pages/Welcome/index.jsx
+++ src/pages/Welcome/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 
 const Welcome = () => {
   const { user, logOut } = useAuth();
+  const navigate = useNavigate();
 
   const handleLogOut = async () => {
     try {
       await logOut();
+      navigate("/");
     } catch (err) {
       console.log(err.message);
     }
