5fed66f3b4555f2e73869a488263a24a18973a35
“crlnvls” <cveloso@hotmail.co.uk>
2023-05-27 20:55:30 +0100

    Add protected route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 
 import * as Pages from "./pages";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
   return (
@@ -12,7 +13,14 @@ const App = () => {
           <Route path="/" element={<Pages.Home />}></Route>
           <Route path="/register" element={<Pages.Register />}></Route>
           <Route path="/login" element={<Pages.Login />}></Route>
-          <Route path="/welcome" element={<Pages.Welcome />}></Route>
+          <Route
+            path="/welcome"
+            element={
+              <ProtectedRoute>
+                <Pages.Welcome />
+              </ProtectedRoute>
+            }
+          ></Route>
           <Route path="*" element={<Pages.NotFound />}></Route>
         </Routes>
       </AuthProvider>
