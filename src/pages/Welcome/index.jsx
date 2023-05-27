ce3ad326270731ccafa862cdbbab04a7cb428582
“crlnvls” <cveloso@hotmail.co.uk>
2023-05-27 20:38:07 +0100

    Add log out functionality

--- src/pages/Welcome/index.jsx
+++ src/pages/Welcome/index.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { useAuth } from "../../context/AuthContext";
 
 const Welcome = () => {
+  const { user, logOut } = useAuth();
+
+  const handleLogOut = async () => {
+    try {
+      await logOut();
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
   return (
     <>
       <h1>Welcome</h1>
+      <br />
+      {user && user.email}
+      <button onClick={handleLogOut}>Log Out</button>
     </>
   );
 };
