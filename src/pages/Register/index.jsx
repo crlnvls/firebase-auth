2e79564ae2087b5a91f8c42ba6ac13f35b0ec521
“crlnvls” <cveloso@hotmail.co.uk>
2023-05-27 20:49:55 +0100

    Add AuthForm to register page

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
+import AuthForm from "../../components/AuthForm";
+import { useAuth } from "../../context/AuthContext";
+
 const Register = () => {
+  const { register } = useAuth();
+
   return (
     <>
-      <h1>Register</h1>
+      <AuthForm
+        loginOrRegister={register}
+        title={"Register"}
+        button={"Create account"}
+        accountQuestion={"Already have an account?"}
+        path={"/login"}
+        link={"Login"}
+      />
     </>
   );
 };
