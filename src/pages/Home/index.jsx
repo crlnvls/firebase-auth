3d81ac75e9b92fc81a995194c868fe7175a75cd7
“crlnvls” <cveloso@hotmail.co.uk>
2023-05-29 16:25:46 +0100

    Set up Tailwind css

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <>
-      <h1>Home</h1>
+      <h1 className="text-green-600">Home</h1>
       <Link to="/login">Login</Link>
       <br />
       <Link to="/register">Register</Link>
